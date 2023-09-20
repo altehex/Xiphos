97525dfe2e07640ff4522a59db230a05937a5f88
alte_hex <helvetikadaufel@gmail.com>
2023-09-20 14:00:28 +0800

    Added uefi.h, updated types.h

--- include/types.h
+++ include/types.h
@@ -1,21 +1,24 @@
 #ifndef _TYPES_H_
 #define _TYPES_H_
 
+/*
+ * Integer types.
+ */
+
 /* Fixed-size types */
 typedef unsigned char           U8;
 typedef unsigned short int      U16;
 typedef unsigned int            U32;
+typedef unsigned long int       U64;
 
 typedef signed char             I8;
 typedef signed short int        I16;
 typedef signed int              I32;
-
-#if __WORDSIZE == 64
-typedef unsigned long int       U64;
 typedef signed long int         I64;
-#else
-typedef unsigned long long int  U64;
-typedef signed long long int    I64;
-#endif 
+
+#ifdef __SIZEOF_INT128__
+typedef unsigned __int128   U128;
+typedef signed __int128     I128;
+#endif
 
 #endif /* !_TYPES_H_ */
